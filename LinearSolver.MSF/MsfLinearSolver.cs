7acad56bbc4a1257f6702e9a7a368ad6a363d6e6
Thomas <thomas_diemar@hotmail.com>
2025-11-23 22:00:22 +0100

    Use MsfLinearSolver and refactor MSF optimiser

--- LinearSolver.MSF/MsfLinearSolver.cs
+++ LinearSolver.MSF/MsfLinearSolver.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using Microsoft.SolverFoundation.Services;
+using LinearSolver;
 
 namespace LinearSolver.MSF
 {
@@ -6,7 +8,44 @@ namespace LinearSolver.MSF
     {
         public double[] Solve(double[,] coefficients, double[] constants)
         {
-            throw new NotImplementedException();
+            if (coefficients == null)
+                throw new ArgumentNullException(nameof(coefficients));
+            if (constants == null)
+                throw new ArgumentNullException(nameof(constants));
+
+            int rows = coefficients.GetLength(0);
+            int cols = coefficients.GetLength(1);
+
+            if (constants.Length != rows)
+                throw new ArgumentException("Constants vector dimension mismatch.", nameof(constants));
+
+            var context = SolverContext.GetContext();
+            context.ClearModel();
+            var model = context.CreateModel();
+
+            Decision[] decisions = new Decision[cols];
+            for (int i = 0; i < cols; i++)
+            {
+                decisions[i] = new Decision(Domain.RealRange(0, 1), $"x{i}");
+                model.AddDecision(decisions[i]);
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                Term lhs = 0;
+                for (int col = 0; col < cols; col++)
+                    lhs += coefficients[row, col] * decisions[col];
+                model.AddConstraint($"Eq_{row}", lhs == constants[row]);
+            }
+
+            model.AddGoal("MinZero", GoalKind.Minimize, 0);
+            context.Solve();
+
+            var result = new double[cols];
+            for (int i = 0; i < cols; i++)
+                result[i] = decisions[i].ToDouble();
+
+            return result;
         }
     }
 }
