4711049f8fdfa9df1599eab3ad587527d311b318
Thomas <thomas_diemar@hotmail.com>
2026-01-01 22:34:57 +0100

    Implement dynamic assembly version from branch name; restrict CI to version* branches

--- RCS.MSF/Properties/AssemblyInfo.cs
+++ RCS.MSF/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,3 +31,5 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
